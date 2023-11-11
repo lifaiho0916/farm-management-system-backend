be4491528200b60eb03739d666a896cf394cab08
Orsonjose Yu <yursonjose@gmail.com>
2023-11-11 13:30:47 -0500

    update commit

--- src/main/java/com/bezkoder/spring/login/controllers/AuthController.java
+++ src/main/java/com/bezkoder/spring/login/controllers/AuthController.java
@@ -80,6 +80,7 @@ public class AuthController {
 
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+      System.out.println("her--------------");
     if (userRepository.existsByUsername(signUpRequest.getUsername())) {
       return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
     }
