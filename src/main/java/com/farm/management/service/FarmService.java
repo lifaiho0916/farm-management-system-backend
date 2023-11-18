0eb11d24a863d2480bdd2db3f134ade2744c3dcb
Orsonjose Yu <yursonjose@gmail.com>
2023-11-18 00:32:52 -0500

    update farm

--- src/main/java/com/farm/management/service/FarmService.java
+++ src/main/java/com/farm/management/service/FarmService.java
@@ -1,7 +1,19 @@
 package com.farm.management.service;
 
 
-public class FarmService {
+import com.farm.management.model.Farm;
 
+import java.util.List;
 
+public interface FarmService {
+
+    Farm createFarm(Farm farm);
+
+    Farm getFarmById(Long farmId);
+
+    List<Farm> getAllFarms();
+
+    Farm updateFarm(Farm farm);
+
+    void deleteFarm(Long farmId);
 }
--- src/main/java/com/farm/management/service/impl/FarmServiceImpl.java
+++ src/main/java/com/farm/management/service/impl/FarmServiceImpl.java
@@ -1,6 +1,53 @@
 package com.farm.management.service.impl;
 
+import com.farm.management.model.Farm;
+import com.farm.management.repository.FarmRepository;
 import com.farm.management.service.FarmService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Service;
 
-public class FarmServiceImpl extends FarmService {}
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Data
+@AllArgsConstructor
+public class FarmServiceImpl implements FarmService {
+
+    private FarmRepository farmRepository;
+
+    @Override
+    public Farm createFarm(Farm farm) {
+        return farmRepository.save(farm);
+    }
+
+    @Override
+    public Farm getFarmById(Long farmId) {
+        Optional<Farm> optionalFarm = farmRepository.findById(farmId);
+        return optionalFarm.get();
+    }
+
+    @Override
+    public List<Farm> getAllFarms() {
+        return farmRepository.findAll();
+    }
+
+    @Override
+    public Farm updateFarm(Farm farm) {
+        Farm existingFarm = farmRepository.findById(farm.getId()).get();
+        existingFarm.setDescription(farm.getDescription());
+        existingFarm.setAddress(farm.getAddress());
+        existingFarm.setCity(farm.getCity());
+        existingFarm.setZipcode(farm.getZipcode());
+        existingFarm.setStates(farm.getStates());
+        Farm updatedFarm = farmRepository.save(existingFarm);
+        return updatedFarm;
+    }
+
+    @Override
+    public void deleteFarm(Long farmId) {
+        farmRepository.deleteById(farmId);
+    }
+}
 
